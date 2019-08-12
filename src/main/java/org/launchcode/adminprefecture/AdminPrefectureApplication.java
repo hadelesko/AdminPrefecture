1f3a35581a40b412222b3486bdf50d077752ceb3
Koomi <koomiadeh@gmail.com>
2019-08-12 12:57:40 -0500

    initial commit

---
+++ src/main/java/org/launchcode/adminprefecture/AdminPrefectureApplication.java
@@ -0,0 +1,14 @@
+package org.launchcode.adminprefecture;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class AdminPrefectureApplication {
+
+	public static void main(String[] args) { run(AdminPrefectureApplication.class, args);
+	}
+
+}
